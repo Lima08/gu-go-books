26ff13e81543ce8521a96b8afc40bf8e8221584a
joao.lima08 <joao.lima@bagy.com.br>
2022-11-01 16:58:23 -0300

    Feat: Add base structure

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img alt="Capa do Livro" />
+        <h2>Título</h2>
+        <h3>Autor</h3>
+    </div>
+
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'CardComponent',
+    components: {
+    }
+  });
+  </script>
+  
+  
+  
